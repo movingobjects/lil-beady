c5be977118d701ee64f5158dd0d154a4967a42f0
Scott Thiessen <scotttt@amazon.com>
2020-03-21 14:38:06 -0700

    Add back to dashboard button

--- src/components/DesignView/index.js
+++ src/components/DesignView/index.js
@@ -7,10 +7,25 @@ import Toolbar from './Toolbar';
 
 class DesignView extends React.Component {
 
+  onBack = () => {
+
+    this.props.dispatch({
+      type: 'setMode',
+      mode: 'dashboard'
+    })
+
+  }
+
   render() {
 
     return (
       <section id='design-view'>
+        <div
+          className='menu'>
+          <button
+            className='back'
+            onClick={this.onBack} />
+        </div>
         <Beads />
         <Toolbar />
       </section>
