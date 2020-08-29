604e099c64b6b745a00bf0926fdeb9d3d8b5d137
Scott Thiessen <scott@movingobjects.io>
2020-08-29 10:15:21 -0700

    Clean up, remove excess styles from dashboard
    Use anchor links when appropriate

--- src/components/App/Dashboard/index.js
+++ src/components/App/Dashboard/index.js
@@ -4,38 +4,6 @@ import { random } from 'varyd-utils';
 
 class Dashboard extends React.Component {
 
-  onProjectCreateClick = () => {
-
-    window.location.hash = `#/dashboard/create`;
-
-  }
-
-  onProjectSelect = (id) => {
-
-    const {
-      projects,
-      templates
-    } = this.props;
-
-    const project = projects.find((p) => p.id === id);
-
-    if (!project) return;
-
-    window.location.hash = `#/project/${project.id}`;
-
-  }
-
-  onBeadCreate = () => {
-
-    window.location.hash = `#/dashboard/bead/create`;
-
-  }
-  onBeadSelect = (id) => {
-
-    window.location.hash = `#/dashboard/bead/edit/${id}`;
-
-  }
-
   render() {
 
     const {
@@ -45,7 +13,7 @@ class Dashboard extends React.Component {
     } = this.props;
 
     return (
-      <section id='dashboard-view'>
+      <section id='dashboard'>
 
         <h1>Lil Beady</h1>
 
@@ -53,15 +21,17 @@ class Dashboard extends React.Component {
         <ul
           className='projects'>
 
-          <li
-            className='create'
-            onClick={this.onProjectCreateClick} />
+          <li>
+            <a href={`#/dashboard/create`}>
+              Create project
+            </a>
+          </li>
 
           {projects.map((project, i) => (
-            <li
-              key={`project-${project.id}`}
-              onClick={() => this.onProjectSelect(project.id)}>
-              {project.name}
+            <li key={project.id}>
+              <a href={`#/project/${project.id}`}>
+                {project.name}
+              </a>
             </li>
           ))}
 
@@ -71,17 +41,24 @@ class Dashboard extends React.Component {
         <ul
           className='beads'>
 
-          <li
-            className='create'
-            onClick={() => this.onBeadCreate()} />
+          <li>
+            <a href={`#/dashboard/bead/create`}>
+              Add Bead
+            </a>
+          </li>
 
           {beads.map((bead, i) => (
             <li
-              key={`bead-${bead.id}`}
-              style={{
-                backgroundColor: bead.color
-              }}
-              onClick={() => this.onBeadSelect(bead.id)} />
+              key={bead.id}>
+              <a
+                style={{
+                  backgroundColor: bead.color
+                }}
+                href={`#/dashboard/bead/edit/${bead.id}`}>
+                {bead.name}
+              </a>
+
+            </li>
           ))}
         </ul>
 
