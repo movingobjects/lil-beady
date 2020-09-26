876eac0b03e65a89ad3ec0a9daf1cfc7412febf1
Scott Thiessen <scotttt@amazon.com>
2020-09-26 03:21:34 -0700

    Update dashboard gradients to use weighted color amounts

--- src/components/App/Dashboard/ProjectCard/index.js
+++ src/components/App/Dashboard/ProjectCard/index.js
@@ -13,22 +13,22 @@ export default class ProjectCard extends React.Component {
 
   }
 
-  getBgColors(count = 2) {
+  getWeightedProjectColors(count) {
 
     const {
       beads,
       project
     } = this.props;
 
-    const weightedColors = [];
+    let weighted = [];
 
     project.design.forEach(({ color }) => {
       if (!!color?.length) {
-        const wColor = weightedColors.find((c) => c.color === color);
+        const wColor = weighted.find((c) => c.color === color);
         if (wColor) {
           wColor.count++;
         } else {
-          weightedColors.push({
+          weighted.push({
             color,
             count: 1
           })
@@ -36,35 +36,70 @@ export default class ProjectCard extends React.Component {
       }
     });
 
-    let bgColors = weightedColors
-      .sort((a, b) => b.count - a.count)
-      .map((wc) => wc.color)
-      .filter((c) => !!c);
+    weighted.sort((a, b) => b.count - a.count);
 
-    if (!bgColors.length) {
-      bgColors = ['white'];
+    if (weighted.length < 2) {
+      weighted = weighted.concat(weighted);
     }
 
-    while (bgColors.length < count) {
-      bgColors = bgColors.concat(bgColors);
-    }
+    const totalCount = weighted.reduce((sum, wc) => sum + wc.count, 0);
 
-    return bgColors.slice(0, count);
+    weighted = weighted.map((wc) => ({
+      ...wc,
+      perc: wc.count / totalCount
+    }))
+
+    return weighted.slice(0, count)
 
   }
 
-  colorsAreBright(bgColors, threshold = 0.75) {
+  getBgStyle() {
 
-    const total = bgColors.reduce((sum, cur) => {
+    const weightedColors  = this.getWeightedProjectColors(3),
+          gradientStyle   = this.getBgGradient(weightedColors),
+          isBgBright      = this.colorsAreBright(weightedColors),
+          textShadowColor = isBgBright ? `rgba(255, 255, 255, 0.25)` : `rgba(0, 0, 0, 0.25)`;
 
-      const color = colors.fromHex(cur),
+    return {
+      backgroundImage: gradientStyle,
+      color: isBgBright ? 'black' : 'white',
+      textShadow: `1px 1px 0 ${textShadowColor}`
+    };
+
+  }
+
+  getBgGradient(weightedColors) {
+
+    let style   = `linear-gradient(to bottom right, `,
+        accPerc = 0;
+
+    weightedColors.forEach((wc, index) => {
+
+      accPerc += wc.perc;
+      style   += `${wc.color}`;
+
+      if (index < weightedColors.length - 1) {
+        style += `, ${Math.round(accPerc * 100)}%, `
+      }
+
+    });
+
+    style += `)`;
+
+    return style;
+
+  }
+
+  colorsAreBright(weightedColors, threshold = 0.75) {
+
+    const avg = weightedColors.reduce((sum, wc) => {
+
+      const color      = colors.fromHex(wc.color),
             brightness = colors.brightness(color);
 
-      return sum + brightness;
+      return sum + (brightness * wc.perc);
 
     }, 0);
-
-    const avg = total / bgColors.length;
 
     return avg > threshold;
 
@@ -76,19 +111,11 @@ export default class ProjectCard extends React.Component {
       project
     } = this.props;
 
-    const bgColors        = this.getBgColors(2),
-          isBgBright      = this.colorsAreBright(bgColors),
-          textShadowColor = isBgBright ? `rgba(255, 255, 255, 0.25)` : `rgba(0, 0, 0, 0.25)`;
-
     return (
       <li
         className={styles.wrap}
         onClick={this.onClick}
-        style={{
-          backgroundImage: `linear-gradient(to bottom right, ${bgColors.join(', ')})`,
-          color: isBgBright ? 'black' : 'white',
-          textShadow: `1px 1px 0 ${textShadowColor}`
-        }}>
+        style={this.getBgStyle()}>
 
         <p>{project.name}</p>
 
